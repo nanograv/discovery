644d08bb72e84387c948cf9c48c131b99deb2c42
Aaron <Aaron9034@Hotmail.com>
2025-01-03 12:38:58 -0800

    Move to ArrayLikelihoods

--- src/discovery/models.py
+++ src/discovery/models.py
@@ -1,51 +1,89 @@
-from jax.tree_util import Partial
+from functools import partial
 import discovery as ds
+from typing import Optional, Callable
 
-def make_likelihood(psrs, noisedict=None, gamma_common=None,
-                red_components=30, common_components=14,
-                common_type='curn', array_like=False):
-    """
-    Construct discovery likelihood object from list of pulsar objects.
+
+def make_array_likelihood(
+    psrs: list[ds.Pulsar],
+    gamma_common: Optional[float]=None,
+    correlation_orf: Optional[Callable]=None,
+    intrinsic_red_components: int=30,
+    common_components: int=14,
+    spectrum: Callable=ds.powerlaw,
+    ) -> ds.ArrayLikelihood:
+    """Construct discovery array likelihood object from a list of Pulsar objects which may contain a noise dictionary.
+
     Parameters:
-    - psrs (list): List of pulsar objects.
-    - noisedict (dict, optional): Dictionary containing noise properties for each pulsar. Defaults to None.
-    - gamma_common (float, optional): Common red noise spectral index. Defaults to None.
-    - red_components (int, optional): Number of red noise components. Defaults to 30.
-    - common_components (int, optional): Number of common noise components. Defaults to 14.
-    - common_type (str, optional): Type of common noise model. Defaults to 'curn'.
-    - array_like (bool, optional): Whether to implement `batched` GPs (experimental). Defaults to False. [Not implemented yet]
+        psrs (list): List of pulsar objects
+        gamma_common (float) [None]: Common red noise spectral index
+        correlation_orf (function, optional) [None]: Correlation overlap reduction function (ORF). Default gives intrinsic red noise only (IRN) model.
+            Options include `ds.uncorrelated_orf`, `ds.hd_orf`, `ds.dipole_orf`, `ds.monopole_orf`, or a custom ORF function
+        intrinsic_red_components (int) [30]: Number of individual pulsar red noise components
+        common_components (int) [14]: Number of common red noise components
+        spectrum (function): Spectrum function for all red noise parameters
+            Options include `ds.powerlaw`, `ds.freespectrum`, or a custom spectrum function
+        tnequad (bool): Use temponest measurement noise definition
 
     Returns:
-    - gl (object): Discovery likelihood object.
+        array_likelihood (object): Discovery ArrayLikelihood object
     """
 
     tspan = ds.getspan(psrs)
 
-    if gamma_common is not None:
-        common_powerlaw = Partial(ds.powerlaw, gamma=gamma_common)
-        gamma_common_name = []
+    if spectrum.__name__ == 'powerlaw':  # powerlaw pieces
+        if gamma_common is not None and correlation_orf is None:
+            print('Warning: gamma_common is set but correlation_orf is None. Setting gamma_common to None.')
+            gamma_common_name = []
+            common_spectrum = None  # just use spectrum in this case
+        elif gamma_common is not None:
+            print(f'Creating powerlaw spectrum red noise model with gamma={gamma_common}.')
+            common_spectrum = ds.makepowerlaw_crn(common_components, crn_gamma=gamma_common)
+            gamma_common_name = []
+        else:
+            print('Creating powerlaw spectrum red noise model with varied gamma.')
+            common_spectrum = ds.makepowerlaw_crn(common_components)
+            gamma_common_name = ['crn_gamma']
+
+        common_names = ['crn_log10_A'] + gamma_common_name
+        components = intrinsic_red_components
+
+    elif spectrum.__name__ == 'freespectrum':  # free spectrum pieces
+        if correlation_orf is None:
+            common_spectrum = ds.makefreespectrum_crn(common_components)
+            components = {'log10_rho': intrinsic_red_components}
+        else:
+            common_spectrum = ds.makefreespectrum_crn(common_components)
+            components = {'log10_rho': intrinsic_red_components, 'crn_log10_rho': common_components}
+
+        common_names = ['crn_log10_rho']
+
     else:
-        common_powerlaw = ds.powerlaw
-        gamma_common_name = ['gw_gamma']
+        raise ValueError('Spectrum function not recognized.')
 
-    if common_type == 'curn':
-        gl = ds.GlobalLikelihood((ds.PulsarLikelihood([psr.residuals,
-                                    ds.makenoise_measurement(psr, noisedict),
-                                    ds.makegp_ecorr(psr, noisedict),
-                                    ds.makegp_timing(psr, svd=True),
-                                    ds.makegp_fourier(psr, ds.powerlaw, red_components, T=tspan, name='red_noise'),
-                                    ds.makegp_fourier(psr, common_powerlaw, common_components, T=tspan,
-                                                      common=['gw_log10_A']+gamma_common_name, name='gw')
-                                                      ]) for psr in psrs))
-    elif common_type == 'hd':
-        gl = ds.GlobalLikelihood((ds.PulsarLikelihood([psr.residuals,
-                                    ds.makenoise_measurement(psr, noisedict),
-                                    ds.makegp_ecorr(psr, noisedict),
-                                    ds.makegp_timing(psr, svd=True),
-                                    ds.makegp_fourier(psr, ds.powerlaw, red_components, T=tspan, name="red_noise")
-                                    ]) for psr in psrs),
-                                    ds.makegp_fourier_global(psrs, common_powerlaw,
-                                                             ds.hd_orf, common_components,
-                                                             T=tspan, name="gw"))
+    # correlation block
+    if correlation_orf is None:  # intrinsic red noise only
+        print('Creating intrinsic red noise only model (IRN).')
+        al = ds.ArrayLikelihood((ds.PulsarLikelihood((psr.residuals,
+                                 ds.makenoise_measurement(psr, psr.noisedict),
+                                 ds.makegp_ecorr(psr, psr.noisedict),
+                                 ds.makegp_timing(psr, svd=True),
+                                 )) for psr in psrs),
+                                 ds.makecommongp_fourier(psrs, spectrum, components=components, T=tspan, name='red_noise'))
+    elif correlation_orf.__name__ == 'uncorrelated_orf':
+        print('Creating uncorrelated red noise model (CURN).')
+        al = ds.ArrayLikelihood((ds.PulsarLikelihood((psr.residuals,
+                                 ds.makenoise_measurement(psr, psr.noisedict),
+                                 ds.makegp_ecorr(psr, psr.noisedict),
+                                 ds.makegp_timing(psr, svd=True),
+                                 )) for psr in psrs),
+                                 ds.makecommongp_fourier(psrs, common_spectrum, intrinsic_red_components, T=tspan, common=common_names, name='red_noise'))
+    else:
+        print(f'Creating {correlation_orf.__name__} correlated red noise model.')
+        al = ds.ArrayLikelihood((ds.PulsarLikelihood((psr.residuals,
+                                 ds.makenoise_measurement(psr, psr.noisedict),
+                                 ds.makegp_ecorr(psr, psr.noisedict),
+                                 ds.makegp_timing(psr, svd=True))) for psr in psrs),
+                                 ds.makecommongp_fourier(psrs, spectrum, intrinsic_red_components, T=tspan, name='red_noise'),
+                                 ds.makegp_fourier_global(psrs, spectrum, correlation_orf, components, T=tspan, name='gw'))
 
-    return gl
+    return al
