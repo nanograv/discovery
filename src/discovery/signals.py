aebcad1a82b321cc49ab6271f158796b86791761
MattM <mattm@ASLNX-BHQ7Z84.vuds.vanderbilt.edu>
2025-08-19 13:29:04 -0500

    attempts at getting chrom injection working

--- src/discovery/signals.py
+++ src/discovery/signals.py
@@ -333,6 +333,21 @@ def fourierbasis_chrom(psr, components, T=None, fref=1400.0):
 
     return f, df, fmatfunc
 
+
+
+def make_fourierbasis_chrom_inj(alpha):
+    def fourierbasis_chrom_inj(psr, components, alpha, T=None, fref=1400.0):
+        f, df, fmat = fourierbasis(psr, components, T)
+
+        fmat, fnorm = matrix.jnparray(fmat), matrix.jnparray(fref / psr.freqs)
+
+        chrom = (fref / psr.freqs)**alpha
+
+        return f, df,  fmat * chrom[:, None]
+    
+	return fourierbasis_chrom_inj
+
+
 def fourierbasis_band_low(psr, components, T=None, fref=1400.0):
     f, df, fmat = fourierbasis(psr, components, T)
 
